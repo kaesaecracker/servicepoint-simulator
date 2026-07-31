ddf354a2ed13e77214666fdc2587536d89fc669e
müde <git@darkest.space>
2026-08-01 01:17:56 +0200

    update to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for servicepoint-simulator";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nix-filter.url = "github:numtide/nix-filter";
     naersk = {
       url = "github:nix-community/naersk";
